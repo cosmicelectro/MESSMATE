2110957852b3d02d554f1fd206a8bdeccc1ec688
Zidan <zidanvai111@gmail.com>
2026-05-31 13:20:10 +0600

    Added gitignore and removed build folder

--- build/Desktop_Qt_6_11_1_MinGW_64_bit-Debug/CMakeFiles/MessMate.dir/MessMate_autogen/EWIEGA46WW/qrc_resources.cpp.obj.d
+++
@@ -1,2 +0,0 @@
-CMakeFiles/MessMate.dir/MessMate_autogen/EWIEGA46WW/qrc_resources.cpp.obj: \
- E:\project\ Messmate\MessMate\build\Desktop_Qt_6_11_1_MinGW_64_bit-Debug\MessMate_autogen\EWIEGA46WW\qrc_resources.cpp
